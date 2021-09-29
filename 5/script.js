7e7076440589592a7a7c711358a8699e3b16f0fe
puzzir <puzz@ya.ru>
2021-09-29 14:59:40 +0300

    lesson 2

---
+++ 5/script.js
@@ -0,0 +1,17 @@
+function addition (a, b) {
+    return (a + b);
+}
+
+function subtract (a, b) {
+    return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide (a, b) {
+    return (a / b);
+}
+
+alert(addition(1,2));
